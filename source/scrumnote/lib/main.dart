c34dfee330ad4e2dd9d834e73bb25b2d5344ec15
= <=>
2019-08-29 15:31:01 +0700

    branch 2 commit

--- source/scrumnote/lib/main.dart
+++ source/scrumnote/lib/main.dart
@@ -23,7 +23,7 @@ class home extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text("Home"),
+        child: listnote(),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: "Ađd",
@@ -34,6 +34,77 @@ class home extends StatelessWidget {
             MaterialPageRoute(builder: (context) => TutorialHome()),
           );
         },
+      ),
+    );
+  }
+}
+
+class listnote extends StatelessWidget{
+  final title = 'Grid List';
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+    title: Text(title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+    trailing: Icon(Icons.keyboard_arrow_right),
+  );
+  @override
+  Widget build(BuildContext context) {
+  return ListView.builder(
+    itemCount: 1,
+    itemBuilder: (context, index) {
+      return ListTile(
+        title: _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        // When a user taps the ListTile, navigate to the DetailScreen.
+        // Notice that you're not only creating a DetailScreen, you're
+        // also passing the current todo through to it.
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TutorialHome(todo: "Le van tri"),
+            ),
+          );
+        },
+      );
+    },
+//    children: [
+//      _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+//      _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+//      _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+//      _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+//      _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+//          Icons.theaters),
+//      _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+//      Divider(),
+//      _tile('Kescaped_code#39;s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+//      _tile('Emmyescaped_code#39;s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+//      _tile(
+//          'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+//      _tile('La Ciccia', '291 30th St', Icons.restaurant),
+//    ],
+  );
+  }
+}
+
+class listview extends StatefulWidget{
+  @override
+  content createState() => new content();
+}
+
+class content extends State<listview>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: new Center(
+        child: new FutureBuilder(builder: null),
       ),
     );
   }
