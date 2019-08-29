fa7722f67e1adddb3405ffdf3880a3b33e21d242
HaoLaptop <37805188+abcpower123@users.noreply.github.com>
2019-08-29 15:18:14 +0700

    firstcommit

---
+++ source/scrumnote/lib/InsertDeleteEx.dart
@@ -0,0 +1,63 @@
+import 'TaskLib.dart';
+import 'package:flutter/material.dart';
+class MyTestInsertDeleteW extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Saving data'),
+      ),
+      body: Row(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              child: Text('Read'),
+              onPressed: () {
+                _read();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              child: Text('Save'),
+              onPressed: () {
+                _save();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Replace these two methods in the examples that follow
+
+  _read() async {
+
+
+    DatabaseHelper helper = DatabaseHelper.instance;
+    int rowId = 1;
+//
+//    Task task = await helper.queryWord(3);
+//    if (task == null) {
+//      print('read row $rowId: empty');
+//    } else {
+//      print('read row $rowId: tieu de ${task.title} noi dung ${task.content}');
+//    }
+    List<Task> ds= await helper.queryAllTasks();
+    for(int i=0;i<ds.length;i++)
+      print("Id:  ${ds[i].id}, Title ${ds[i].title},Content ${ds[i].content}");
+  }
+
+  _save() async {
+    Task t=new Task(status: 0,title: "cong viec1",content: "khong lam gi ca",dateD: "2019/8/9");
+    print(t.title+" "+t.content);
+
+    DatabaseHelper helper = DatabaseHelper.instance;
+    int id = await helper.insert(t);
+    print('inserted row: $id');
+  }
+}
---
+++ source/scrumnote/lib/TaskLib.dart
@@ -0,0 +1,142 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+// database table and column names
+final String tableTask = 'tasks';
+final String columnId = '_id';
+final String columnSattus = 'status';
+final String columnTitle = 'title';
+final String columnContent = 'content';
+final String columnDateD = 'dated';
+
+// data model class
+class Task {
+
+  int id;
+
+  int status ;
+  String title;
+  String content ;
+  String dateD ;
+
+  Task({int id, int status, String title,String content,String dateD}){
+    this.id=id;
+    this.status=status;
+    this.title=title;
+    this.content=content;
+    this.dateD=dateD;
+  }
+
+
+  // convenience constructor to create a Word object
+  Task.fromMap(Map<String, dynamic> map) {
+
+    id=map[columnId];
+    status=map[columnSattus];
+    title=map[columnTitle];
+    content=map[columnContent];
+    dateD=map[columnDateD];
+  }
+
+  // convenience method to create a Map from this Word object
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+    columnSattus:status,
+    columnTitle:title,
+    columnContent:content,
+   columnDateD:dateD,
+
+
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+}
+
+// singleton class to manage the database
+class DatabaseHelper {
+
+  // This is the actual database filename that is saved in the docs directory.
+  static final _databaseName = "MyDatabase.db";
+  // Increment this version when you need to change the schema.
+  static final _databaseVersion = 1;
+
+  // Make this a singleton class.
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  // Only allow a single open connection to the database.
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  // open the database
+  _initDatabase() async {
+    // The path_provider plugin gets the right directory for Android or iOS.
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    print(path);
+    // Open the database. Can also add an onUpdate callback parameter.
+    return await openDatabase(path,
+        version: _databaseVersion,
+        onCreate: _onCreate);
+  }
+
+  // SQL string to create the database
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+              CREATE TABLE $tableTask (
+                $columnId INTEGER PRIMARY KEY,
+                $columnSattus INTEGER,
+                $columnTitle TEXT NOT NULL,
+                $columnContent TEXT NOT NULL,
+                $columnDateD TEXT NOT NULL
+               
+              )
+              ''');
+  }
+
+  // Database helper methods:
+
+  Future<int> insert(Task task) async {
+    Database db = await database;
+    int id = await db.insert(tableTask, task.toMap());
+    return id;
+  }
+
+  Future<Task> queryWord(int id) async {
+    Database db = await database;
+    List<Map> maps = await db.query(tableTask,
+        columns: [columnId, columnSattus, columnTitle,columnContent,columnDateD],
+        where: '$columnId = ?',
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Task.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<Task>> queryAllTasks() async {
+  
+    Database db = await database;
+    List<Map> maps = await db.query(tableTask,
+        columns: [columnId, columnSattus, columnTitle,columnContent,columnDateD]);
+    if (maps.length > 0) {
+      List<Task> res= new List();
+      for (int i=0;i<maps.length;i++)
+          res.add(Task.fromMap(maps.elementAt(i)));
+      return res;
+    }
+
+    return null;
+  }
+// TODO: delete(int id)
+// TODO: update(Word word)
+}
